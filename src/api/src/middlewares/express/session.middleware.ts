1179db4efa780d4a113b5ac210ae0abc84a0b284
ViniciosLugli <vinicioslugli@gmail.com>
2022-11-16 00:42:53 -0300

    feat(api): :sparkles: Implemented session middleware for express users

---
+++ src/api/src/middlewares/express/session.middleware.ts
@@ -0,0 +1,22 @@
+import { Express } from 'express';
+import { Middleware } from '@middlewares/express';
+import session from 'express-session';
+import Redis from '@services/redis.service';
+import connectRedis from 'connect-redis';
+
+class AuthenticationMiddleware extends Middleware {
+	setup(app: Express): void {
+		const RedisStore = connectRedis(session);
+
+		app.use(
+			session({
+				secret: process.env.SESSION_SECRET || 'secretdev',
+				store: new RedisStore({ client: Redis }),
+				resave: false,
+				saveUninitialized: false,
+			}),
+		);
+	}
+}
+
+export default new AuthenticationMiddleware();
