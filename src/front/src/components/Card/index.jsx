d0067c5b2f59f82fec7c722e9d5f67a87616e861
Henrique Marlon <henriquemarloncs@gmail.com>
2022-11-21 09:56:44 -0300

    feat(): Change default local port and some changes.

--- src/front/src/components/Card/index.jsx
+++ src/front/src/components/Card/index.jsx
@@ -5,7 +5,7 @@ import Monitor from "../../assets/Monitor";
 
 export function Card() {
   const [count, setCount] = useState([
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16
   ]);
 
   return (
