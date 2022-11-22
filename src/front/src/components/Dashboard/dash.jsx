a1362d4ba79b7ee88b0a6a5ffd075aa4afec5384
VZeferino <vitor.queiroz@sou.inteli.edu.br>
2022-11-22 10:41:30 -0300

    Novo componente (Dashboard) e conexão

---
+++ src/front/src/components/Dashboard/dash.jsx
@@ -0,0 +1,10 @@
+import "./dash.css"
+
+export function Dash(){
+  return (
+
+    <div>
+      <h1 className="text">Dashboard</h1>
+    </div>
+  )
+}
--- src/front/src/main.jsx
+++ src/front/src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {Card} from './components/Card'
 import {Navbar} from './components/Navbar/navbar'
+import {Dash} from './components/Dashboard/dash'
 import "./styles/main.css"
 
 let array = ["Prédio 1", "Prédio 2", "Prédio 3", "Prédio 4", "Prédio 5"]
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Dash />
     <Navbar Prédio={array}/>
     <Card />
   </React.StrictMode>
