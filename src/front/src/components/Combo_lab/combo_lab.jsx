1f01a0a936291f3ff8cd7175b6ac11c414129af2
melyssa08 <melyssa.rojas@sou.inteli.edu.br>
2022-11-28 16:32:16 -0300

    Nova atualização do combo lab

--- src/front/src/components/Combo_lab/combo_lab.jsx
+++ src/front/src/components/Combo_lab/combo_lab.jsx
@@ -2,20 +2,23 @@ import './combo_lab.css'
 import {useState} from 'react'
 import Toggle from './toggle'
 
-function Combo_lab(props) {
+function Combo_lab() {
+
+  const [countCards, setCount] = useState([2,3,4,5])
+  const [countDevices, setCountDevices] = useState([5,2,6,7])
 
   var contador = 0
       return  (
         <div className='layout-lab'>
           {
-          props.qtd.cards.map((i) => {
+          countCards.map((i) => {
             return (
             <div className="card-lab" key={`${i}`}>
               <h1 className='name-lab' > Laboratório 4 </h1>
               <Toggle number={`${i}`} />
             <div className='content-card'> 
               <h1 className='content-lab'> 
-              {`${props.qtd.devices[contador]}`}
+              {`${countDevices[contador]}`}
                </h1>
               <h1 className='sub-title-content-lab'> Quantidade de dispositivos </h1>
             {contador = contador + 1}
