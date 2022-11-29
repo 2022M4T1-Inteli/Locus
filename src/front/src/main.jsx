64174309c3eeafab12f6484858caeab23ecebe74
Henrique Marlon <henriquemarloncs@gmail.com>
2022-11-29 09:18:40 -0300

    refactoring[ component ]: Change component name.

--- src/front/src/main.jsx
+++ src/front/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {Card} from './components/Card'
-import {Navbar} from './components/Navbar/navbar'
+import {Navbar} from './components/Navbar/Navbar'
 import {Dash} from './components/Dashboard/dash'
 import {Filter} from './components/Navbar/Filter'
 import "./styles/main.css"
@@ -10,9 +10,13 @@ let array = ["Prédio 1", "Prédio 2", "Prédio 3", "Prédio 4", "Prédio 5"]
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Dash />
-    <Navbar Prédio={array}/>
-    <Filter></Filter>
-    <Card />
+    <div>
+        <Dash />
+        <Navbar Prédio={array}/>
+        <Filter></Filter>
+      <div className='caixapreta'>
+        <Card />
+      </div>
+    </div>
   </React.StrictMode>
 )
