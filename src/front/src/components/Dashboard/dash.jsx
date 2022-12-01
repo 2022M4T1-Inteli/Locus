07aea49e69bd2baa289c3fefaac71a55a04f2584
VZeferino <vitor.queiroz@sou.inteli.edu.br>
2022-12-01 11:18:44 -0300

    Dashboard perfil.

--- src/front/src/components/Dashboard/dash.jsx
+++ src/front/src/components/Dashboard/dash.jsx
@@ -1,10 +1,19 @@
 import "./dash.css"
+import icon from "./icon.png"
+import profile from "./naruto.jpg"
 
 export function Dash(){
   return (
-
-    <div>
+    <div className="display">
       <h1 className="text">Dashboard</h1>
+
+      <div className="userBloc">
+        <img className="imagem" src={icon} alt="Ajuda" />
+        <div>
+          <img className="profile" src={profile} alt="Profile photo" />
+        </div>
+        <h1 className="nome">Zeferino</h1>
+      </div>
     </div>
   )
 }
