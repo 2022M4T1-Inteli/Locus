2162869490ed9a17b49e3589cd3926a0d87d165b
JuliaTogni <julia.togni@sou.inteli.edu.br>
2022-12-01 11:14:33 -0300

    Manutenção
    Melhorias e manutenção do código

--- src/front/src/components/Navbar/Navbar.jsx
+++ src/front/src/components/Navbar/Navbar.jsx
@@ -5,13 +5,17 @@ export function Navbar({Prédio}) {
   return (
 
     <div className="navbar">
+      <div className="div-button">
       {Prédio.map((elem) => {
         return (
           <div key={elem}>
             <button>{elem}</button>
           </div>)
       })}
+      </div>
+      <div>
       <Filter></Filter>
+      </div>
     </div>
   )
 }
