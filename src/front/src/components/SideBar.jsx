dc2063786f7f996b5eeff59ccc63a5c749816432
Henrique Marlon <henriquemarloncs@gmail.com>
2022-12-14 01:24:50 -0300

    feat(): creating sidebar

--- src/front/src/components/SideBar.jsx
+++ src/front/src/components/SideBar.jsx
@@ -8,7 +8,12 @@ export function SideBar() {
                 src="https://images.unsplash.com/photo-1593640408182-31c70c8268f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
             /> */}
             <footer>
-                <a href="#">Dashboard</a>
+                <a href="#">Todos Dipositivos</a>
+                <a href="#">Sala 01</a>
+                <a href="#">Sala 02</a>
+                <a href="#">Sala 03</a>
+                <a href="#">Sala 04</a>
+                <a href="#">Sala 05</a>
             </footer>
         </aside>
     );
